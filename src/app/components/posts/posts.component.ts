419db866a3a543287098a3b94d2b7afc7ecf2b56
William Gibbons <wtg14b@my.fsu.edu>
2020-04-30 23:40:57 -0400

    lots of stuff, weird Http

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/article/article.model';
+import { PostService } from 'src/app/services/post.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  postsHeader = `Reddit Clone Posts`;
+  
+  searchFilter = ``;
+
+  postList: Article[];
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe(postList => this.postList = postList);
+    console.log(this.postList);
+  }
+
+}
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Article } from 'src/app/article/article.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private postListJson = `[
+    {
+      "title": "test",
+      "link": "www.test.com",
+      "votes": 5
+    },
+    {
+      "title": "test2",
+      "link": "www.test2.com",
+      "votes": 10
+    }
+  ]`;
+  
+  private readonly URL = ``; // url to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  postList: Article[];
+
+  public getPosts(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.URL);
+  }
+
+  constructor(private http: HttpClient) { 
+    this.postList = JSON.parse(this.postListJson);
+  }
+}
